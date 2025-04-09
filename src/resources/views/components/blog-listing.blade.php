4aa049114aee23d4cf2744b8b90c3345fc26b6ba
Ketan <ketan.smarttechnica@gmail.com>
2025-04-09 10:08:14 +0530

    component ke atracive banaya he bootstrap se

--- src/resources/views/components/blog-listing.blade.php
+++ src/resources/views/components/blog-listing.blade.php
@@ -1,8 +1,17 @@
-<div>
-    <h2>Blog Listing</h2>
-    <ul>
-        @foreach (\Smartttech\Blog\Models\Blog::all() as $blog)
-            <li><strong>{{ $blog->title }}</strong> - {{ $blog->content }}</li>
-        @endforeach
-    </ul>
+<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-4">
+    @foreach (\Smartttech\Blog\Models\Blog::all() as $blog)
+        <div class="col">
+            <div class="card h-100 shadow-sm border-0 hover-shadow">
+                <div class="card-body">
+                    <h5 class="card-title text-primary">{{ $blog->title }}</h5>
+                    <p class="card-text text-muted">
+                        {{ \Illuminate\Support\Str::limit($blog->content, 100) }}
+                    </p>
+                </div>
+                <div class="card-footer bg-white border-0 text-end">
+                    <a href="#" class="btn btn-sm btn-outline-primary">Read More</a>
+                </div>
+            </div>
+        </div>
+    @endforeach
 </div>
